a4024e38af70d6215b90bf0cbdae126dace8defd
isaac lynch <thebunnyarcher@gmail.com>
2021-09-16 02:33:39 -0400

    2:33, we draw closer to the end.

--- src/main/java/exercise08/solution08.java
+++ src/main/java/exercise08/solution08.java
@@ -4,47 +4,56 @@
  */
 package exercise08;
 
-import java.util.*;
+import java.util.Scanner;
 
+class Pizza {
+    int ppl;
+    int p;
+    int sl;
+
+    public void scn() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many party people? ");
+        this.ppl = sc.nextInt();
+
+        System.out.print("How many silly pizzas do you have? ");
+        this.p = sc.nextInt();
+
+        System.out.print("How many goofy slices per pizza? ");
+        this.sl = sc.nextInt();
+    }
+
+    public void prt() {
+        int t = sl*p;
+
+        System.out.println(ppl +" party people with "+ p +" pizzas ("+ t +" slices)");
+
+        if(t/ppl==1) //print statements for each person getting one slice
+        {
+            System.out.println("Each person gets "+ t/ppl +" piece of the silly pizza.");
+            System.out.println("There are "+ (t%ppl) +" leftover pieces, for later!");
+        }
+
+        else //print statements for more than one slice each
+        {
+            System.out.println("Each person gets "+ t/ppl+" pieces of the silly pizza.");
+            System.out.println("There are "+ (t%ppl) +" leftover pieces, for later!");
+        }
+    }
+}
 public class solution08 {
         public static void main(String[] args) {
-
-            Scanner s = new Scanner(System.in);
-            System.out.print("How many party people? ");
-            int ppl = s.nextInt();
-
-            System.out.print("How many silly pizzas do you have? ");
-            int p = s.nextInt(); //pizza read
-
-            System.out.print("How many goofy slices per pizza? ");
-            int sl = s.nextInt(); //slice read
-
-            int t = sl*p;
-
-            System.out.println(ppl +" party people with "+ p +" pizzas ("+ t +" slices)");
-
-            if(t/ppl==1) //print statements for each person getting one slice
-            {
-                System.out.println("Each person gets "+ t/ppl +" piece of the silly pizza.");
-                System.out.println("There are "+ (t%ppl) +" leftover pieces, for later!");
-            }
-
-            else //print statements for more than one slice each
-            {
-                System.out.println("Each person gets "+ t/ppl+" pieces of the silly pizza.");
-                System.out.println("There are "+ (t%ppl) +" leftover pieces, for later!");
-            }
-
+            Pizza ca = new Pizza();
+            ca.scn();
+            ca.prt();
 
         }
     }
 
-/* psuedocode: the first that happens in this code is that we import java.util.*, after that we make an
-instance of the scanner function to be used to read in the users inputs. then, the number of people, pizzas
-and slices each pizza serves are scanned and stored. the number of total slices is found by multiplying
-the number of pizzas by the number of slices. slices is stored in an int called "sl", pizza in one
-called "p", total slices in "t", and the amount of people in "ppl". the statement relays the information
-given back to the user and then does one of two things with the help of an if/else statement. if the total
-number of slices divided by the number of total people equals one, meaning each person only get one slice,
-then the code prints out "piece". if it's more than 1, then "pieces" is printed out instead.
+/* winds in the east... anywho, first we import the scanner function. the a class titled
+pizza is created which will store the ints  for people, pizzas, and slices of pizza.
+there are also two void functions inside the class. one is "scn" which scans in the
+data the user gives and the "prt" which does the math and prints out the solutions.
+the main function just calls these two public voids to run the program.
  */
